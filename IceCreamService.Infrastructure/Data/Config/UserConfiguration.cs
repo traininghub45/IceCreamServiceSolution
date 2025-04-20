f0606764d3967fdf496c0c7d157e5b97f7faf925
Oday Alsanad <odayalsanad6374@gmail.com>
2025-04-20 22:09:17 +0300

    BuilderAssembly

---
+++ IceCreamService.Infrastructure/Data/Config/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using IceCreamService.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IceCreamService.Infrastructure.Data.Config
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Username).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.FullName).IsRequired().HasMaxLength(100);
+            builder.Property(e => e.Password).IsRequired().HasColumnName("PasswordHash");
+        }
+    }
+}
